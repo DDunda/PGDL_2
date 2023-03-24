8d51325b33569a08ba3a4dd1f4ed6906ae09f4e3
R0binicus <102570571+R0binicus@users.noreply.github.com>
2023-03-24 23:39:36 +1100

    Tried to add wall bounce (but failed)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private LayerMask jumpableGround;
-    [SerializeField] private float speed = 10;
-    private Rigidbody2D body;
+    [SerializeField] private float walkSpeed = 10;
+    [SerializeField] private bool jumpKingJump = true;
+    [SerializeField] private float jumpForce = 0.0f;
+    private bool canJump = true;
+    private Rigidbody2D rb;
     //private Animator anim;
     private BoxCollider2D collider;
+    private PhysicsMaterial2D bounceMat, normalMat;
+
     //private bool grounded;
 
 
@@ -16,7 +21,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        body = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         collider = GetComponent<BoxCollider2D>();
         //anim = GetComponent<Animator>();
     }
@@ -24,11 +29,22 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        
+        
+        
+        if(jumpForce == 0.0f && IsGrounded() && jumpKingJump)
+        {
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * walkSpeed, rb.velocity.y);
+        } else if(!jumpKingJump)
+        {
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * walkSpeed, rb.velocity.y);
+        }
+        
+        
 
         //Flip player sprite when moving left or right
-        if(horizontalInput > 0.01f)
+        if(horizontalInput >= 0.01f)
         {
             transform.localScale = Vector3.one;
         } else if(horizontalInput < -0.01f)
@@ -36,16 +52,84 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        if(Input.GetKey(KeyCode.Space) && IsGrounded())
+        if(Input.GetKey(KeyCode.Space) && IsGrounded() && !jumpKingJump)
         {
             Jump();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded() && jumpKingJump && canJump)
+        {
+            rb.velocity = new Vector2(0.0f, rb.velocity.y);
+        }
+
+        if(Input.GetKey(KeyCode.Space) && IsGrounded() && jumpKingJump && canJump && jumpForce < 20f)
+        {
+            jumpForce += 0.1f;
+        }
+
+        /*if((jumpForce >= 20.1f) && IsGrounded() && jumpKingJump)
+        {
+            float tempx = horizontalInput * walkSpeed;
+            float tempy = jumpForce;
+            rb.velocity = new Vector2(tempx, tempy);
+            Invoke("ResetJump", 0.2f);
+        }
+        */
+
+        if(jumpForce >= 0 && !IsGrounded()) 
+        {
+            rb.sharedMaterial = bounceMat;
+        }else
+        {
+            rb.sharedMaterial = bounceMat;
+            //rb.sharedMaterial = normalMat;
+        }
+
+        //when space key is released
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            if(IsGrounded())
+            {
+                rb.velocity = new Vector2(horizontalInput * walkSpeed, jumpForce);
+                jumpForce = 0.0f;
+            }
+            canJump = true;
         }
     }
 
     private void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x, speed);
-        //grounded = false;
+        rb.velocity = new Vector2(rb.velocity.x, walkSpeed);
+        /*float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if(Input.GetKey(KeyCode.Space) && IsGrounded())
+        {
+            jumpForce += 0.5f;
+            rb.velocity = new Vector2(0.0f, rb.velocity.y);
+            rb.sharedMaterial = bounceMat;
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+        if (Input.GetKey(KeyCode.Space) && IsGrounded() && jumpForce >= 20.0f || Input.GetKey(KeyCode.Space) == false && jumpForce >= 0.1f)
+        {
+            float tempX = horizontalInput * walkSpeed;
+            float tempY = jumpForce;
+            rb.velocity = new Vector2(tempX,tempY);
+            Invoke("ResetJump", 0.025f);  
+        }
+        if (rb.velocity.y <= -1)
+        {
+            rb.sharedMaterial = normalMat;
+        }
+        */
+    }
+
+    private void ResetJump()
+    {
+        canJump = false;
+        jumpForce = 0;
     }
 
     private bool IsGrounded()
